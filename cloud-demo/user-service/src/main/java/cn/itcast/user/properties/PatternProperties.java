da55971de3d8b5e89c4691b82882f233e6f53426
lpruoyu <2975064528@qq.com>
2022-08-08 20:07:57 +0800

    2022-08-08 Nacos discovery config end

---
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/properties/PatternProperties.java
@@ -0,0 +1,12 @@
+package cn.itcast.user.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+    private String dateformat;
+}
--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,18 +1,42 @@
 package cn.itcast.user.web;
 
 import cn.itcast.user.pojo.User;
+import cn.itcast.user.properties.PatternProperties;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
+//@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PatternProperties properties;
+
+    // 注入nacos中的配置属性
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+
+    // 编写controller，通过日期格式化器来格式化现在时间并返回
+    @GetMapping("now")
+    public String now() {
+        return LocalDateTime.now().format(
+                DateTimeFormatter.ofPattern(properties.getDateformat())
+        );
+    }
+
 
     /**
      * 路径： /user/110
